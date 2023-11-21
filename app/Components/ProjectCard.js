8d7209905f3e63638b3d627440e3c3c1837b36a6
Rahul Gupta <iamrahulgupta4u@gmail.com>
2023-11-21 12:48:25 +0530

    Video on Hover and toast

--- app/Components/ProjectCard.js
+++ app/Components/ProjectCard.js
@@ -1,60 +1,95 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-
+import { useRef, useState } from "react";
+import { Toaster, toast } from "sonner";
 function ProjectCard({ project }) {
+  const [isVideo, setVideo] = useState(false);
+  const videoRef = useRef(null);
+  const handleHover = () => {
+    const video = videoRef.current;
+    video.play();
+  };
+
   return (
-    <div className='basis-1/4 flex-1 border-2 rounded-md  justify-center  border-black shadow-md hover:shadow-slate-500  overflow-hidden'>
-      <Image
-        alt={"img"}
-        src={project.imgurl}
-        width={800}
-        height={800}
-        objectFit='contain'
-        className='rounded-t-md overflow-hidden object-contain flex-1 '
-      />
-      <div className='w-full p-4 text-black justify-between '>
-        <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-mono font-semibold '>
+    <div className="basis-1/4 flex-1 border-2 rounded-md  justify-center  border-black shadow-md hover:shadow-slate-500  overflow-hidden">
+      <div className="h-60">
+        {!isVideo && (
+          <Image
+            alt={"img"}
+            src={project.imgurl}
+            width={800}
+            height={800}
+            className={`rounded-t-md overflow-hidden object-contain flex-1 `}
+            onMouseEnter={() => setVideo(true)}
+          />
+        )}
+        {isVideo && (
+          <>
+            <video
+              width="600"
+              ref={videoRef}
+              onMouseEnter={handleHover}
+              onMouseLeave={() => {
+                videoRef.current.pause();
+                setVideo(false);
+              }}
+              autoPlay
+              height="600"
+            >
+              <source src={project.videoLink} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </>
+        )}
+      </div>
+      <div className="w-full p-4 text-black justify-between ">
+        <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-mono font-semibold ">
           {project.title}
         </h3>
-        <p class='flex flex-row flex-wrap gap-2 mb-5 items-center  justify-start text-xs md:text-sm '>
+        <p class="flex flex-row flex-wrap gap-2 mb-5 items-center  justify-start text-xs md:text-sm ">
           {project.tags.map((tag) => (
             <span
               key={tag}
-              class='inline-block px-2 py-1 font-semibold border-2 cursor-pointer hover:bg-black hover:text-white  border-stone-900  rounded-md'
+              class="inline-block px-2 py-1 font-semibold border-2 cursor-pointer hover:bg-black hover:text-white  border-stone-900  rounded-md"
             >
               {tag}
             </span>
           ))}
         </p>
-        <p className='flex flex-wrap gap-2 flex-row items-center  justify-start text-xs md:text-sm '>
+        <p className="flex flex-wrap gap-2 flex-row items-center  justify-start text-xs md:text-sm ">
           <a
-            target='_blank'
+            target="_blank"
             href={project.github}
-            className='relative inline-block text-lg group'
+            className="relative inline-block text-lg group"
           >
-            <span className='relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white'>
-              <span className='absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50'></span>
-              <span className='absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease'></span>
-              <span className='relative'>Github</span>
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span className="relative">Github</span>
             </span>
             <span
-              className='absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0'
-              data-rounded='rounded-lg'
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
             ></span>
           </a>
           <a
-            target='_blank'
+            target="_blank"
             href={project.linkedIn}
-            className='relative inline-block text-lg group'
+            className="relative inline-block text-lg group"
+            onMouseEnter={() =>
+              toast.info("Go to LinkedIn for more Details and Live Site")
+            }
           >
-            <span className='relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white'>
-              <span className='absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50'></span>
-              <span className='absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease'></span>
-              <span className='relative'>LinkedIn</span>
+            <Toaster duration={2000} />
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span className="relative">LinkedIn</span>
             </span>
             <span
-              className='absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0'
-              data-rounded='rounded-lg'
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
             ></span>
           </a>
         </p>
--- app/Components/ProjectsData.js
+++ app/Components/ProjectsData.js
@@ -4,11 +4,35 @@ import ProjectCard from "./ProjectCard";
 import TabsDefault from "./Tabs";
 const projects = [
   {
+    id: 7,
+    title: "Issue Tracker ",
+    imgurl: "/Issue-tracker.png",
+    tags: ["React", "Next JS", "Tailwind", "Prisma", "Mongodb", "Radix UI"],
+    github: "https://github.com/Iamrahul4u/issue-tracker",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1700546730/Issue_Tracker_-_Brave_2023-10-21_01-59-38_2_qjmsa7.mp4",
+    linkedIn:
+      "https://www.linkedin.com/feed/update/urn:li:activity:7121342586966949888/",
+  },
+  {
+    id: 8,
+    title: "DevFlow",
+    imgurl: "/devflow.png",
+    tags: ["React", "Next JS", "Tailwind", "Mongodb", "ShadCn", "Clerk"],
+    github: "https://github.com/Iamrahul4u/NextJs-Jsm",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1700546770/DevFlow_-_Brave_2023-11-21_09-47-57_2_2_tdlmyi.mp4",
+    linkedIn:
+      "https://www.linkedin.com/feed/update/urn:li:activity:7132593754460307456/",
+  },
+  {
     id: 1,
     title: "Fast React Pizza",
     imgurl: "/web1.png",
     tags: ["React", "Redux Toolkit", "React Router", "Tailwind", "Api"],
     github: "https://github.com/Iamrahul4u/fastfood-redux-react-app",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1696369986/PizzaOrder_wtasxl.mp4",
     linkedIn:
       "https://www.linkedin.com/feed/update/urn:li:activity:7105065605614977024/",
   },
@@ -19,6 +43,8 @@ const projects = [
     imgurl: "/web5.png",
     tags: ["React", "React Router", "Context Api", "Leaflet"],
     github: "https://github.com/Iamrahul4u/worldwise-react-project",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1696369987/WorldWise_cahpt8.mp4",
     linkedIn:
       "https://www.linkedin.com/feed/update/urn:li:activity:7103430677244604416/",
   },
@@ -28,6 +54,8 @@ const projects = [
     imgurl: "/web4.png",
     tags: ["React", "JSON-Server", "Context Api"],
     github: "https://github.com/Iamrahul4u/Quiz-app",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1696369984/ReactQuiz_aurumy.mp4",
     linkedIn:
       "https://www.linkedin.com/feed/update/urn:li:activity:7100092430221291520/",
   },
@@ -37,6 +65,8 @@ const projects = [
     imgurl: "/web3.png",
     tags: ["React", "Tailwind", "Omdb Api", "Context Api"],
     github: "https://github.com/Iamrahul4u/usePopCorm",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1696369986/UsePopcorm_lbj0c0.mp4",
     linkedIn:
       "https://www.linkedin.com/feed/update/urn:li:activity:7099129333805383680/",
   },
@@ -46,6 +76,8 @@ const projects = [
     imgurl: "/web2.png",
     tags: ["React", "Tailwind"],
     github: "https://github.com/Iamrahul4u/far-away",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1696369988/FarAway_ubbg6v.mp4",
     linkedIn: "https://www.linkedin.com/in/iamrahul4u/",
   },
   {
@@ -54,17 +86,10 @@ const projects = [
     imgurl: "/web6.png",
     tags: ["React", "Next JS", "Tailwind"],
     github: "https://github.com/Iamrahul4u/Portfolio",
+    videoLink:
+      "https://res.cloudinary.com/duavblp7s/video/upload/v1700546799/Portfolio_-_Brave_2023-10-06_01-38-33_wy8yjz.mp4",
     linkedIn:
       "https://www.linkedin.com/feed/update/urn:li:activity:7115196017913819136/",
-  },
-  {
-    id: 7,
-    title: "Issue Tracker ",
-    imgurl: "/Issue-tracker.png",
-    tags: ["React", "Next JS", "Tailwind", "Prisma", "Mongodb", "Radix UI"],
-    github: "https://github.com/Iamrahul4u/issue-tracker",
-    linkedIn:
-      "https://www.linkedin.com/feed/update/urn:li:activity:7121342586966949888/",
   },
 ];
 const filter = [
@@ -76,6 +101,7 @@ const filter = [
   "Api",
   "Mongodb",
   "Prisma",
+  "Clerk",
 ];
 
 const ProjectsData = () => {
@@ -89,7 +115,7 @@ const ProjectsData = () => {
   return (
     <>
       <TabsDefault filters={filter} handleClick={handleClick} active={active} />
-      <ul className='flex flex-col lg:grid lg:grid-cols-3  py-10 gap-10'>
+      <ul className="flex flex-col lg:grid lg:grid-cols-2  py-10 gap-10">
         {filteredData.map((project) => {
           return <ProjectCard key={project.id} project={project} />;
         })}
