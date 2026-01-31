e28caff588e804b40a9344bfc5132859de804a50
Rahul Gupta <iamrahulgupta4u@gmail.com>
2026-02-01 00:41:42 +0530

    Update Sparkles.tsx with new content

--- app/Components/Sparkles.tsx
+++ app/Components/Sparkles.tsx
@@ -6,7 +6,7 @@ export default function SparklesPreview({ children }) {
   return (
     <div className="h-[25rem] relative  w-full  bg-black flex flex-col items-center justify-start overflow-hidden  rounded-md">
       <h1 className="md:text-7xl text-5xl  lg:text-9xl font-bold text-center text-white relative z-[500] ">
-        Rahul Gupta
+        Shaikh Najeeb
       </h1>
       <div className="md:w-[50rem] w-[30rem] h-40 mt-1 md:mt-0 relative">
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
