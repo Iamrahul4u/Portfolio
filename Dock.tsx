c5be340695c5e0645b70d9f4ac5d9a17e3d01bf9
Rahul Gupta <iamrahulgupta4u@gmail.com>
2026-02-01 00:53:45 +0530

    Update contact information in Dock.tsx

---
+++ Dock.tsx
@@ -0,0 +1,9 @@
+// Updated contact details
+
+const contact = {
+    email: 'Shaikhnajeeb001@gmail.com',
+    linkedIn: 'https://www.linkedin.com/in/shaikh-najeeb',
+    whatsApp: 'https://wa.me/8767547779'
+};
+
+export default contact;
